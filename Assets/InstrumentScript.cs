ff86471e5584c59bacc40ac58e638d2f362b31e1
DiepSigh <Inuyasha768@hotmail.com>
2020-08-30 21:52:54 -0400

    Clean up and added script
    removed unused sounds and unity banner script, added gong and cymbal mp3, added script for switching between instruments

---
+++ Assets/InstrumentScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstrumentScript : MonoBehaviour
+{
+
+    public int value;
+    public Text instrumentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = 0;
+        instrumentText.text = "Drum";
+    }
+
+    public void changeInstrument(){
+        value++;
+        if (value==3){
+            Start();
+        }else if (value==2){
+            instrumentText.text = "Gong";
+        }else{ //if value == 1
+            instrumentText.text = "Cymbals";
+        }
+    }
+}
