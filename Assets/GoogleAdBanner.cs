d61a3cfa50e7eebefef29e68e5f9581fb3da0d47
DiepSigh <Inuyasha768@hotmail.com>
2024-07-06 20:31:46 -0400

    Project Overhaul to 2022.3.35f1
    Started new project on new version instead of upgrading

--- Assets/GoogleAdBanner.cs
+++ Assets/GoogleAdBanner.cs
@@ -1,37 +1,112 @@
 ﻿using System;
 using UnityEngine;
+using GoogleMobileAds;
 using GoogleMobileAds.Api;
 
 public class GoogleAdBanner : MonoBehaviour
 {
-    private BannerView bannerView;
-    
+
     public void Start()
     {
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(initStatus => { });
-
-        this.RequestBanner();
+        CreateBannerView();
     }
+    
+    #if UNITY_ANDROID
+        string _adUnitId = "ca-app-pub-7760662598407173/2927518920";
+    #elif UNITY_IPHONE
+        string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+    #else
+        string _adUnitId = "unused";
+    #endif
 
-    private void RequestBanner()
+    BannerView _bannerView;
+
+    public void CreateBannerView()
     {
-        #if UNITY_ANDROID
-            string adUnitId = "ca-app-pub-7760662598407173/2927518920";
-        #elif UNITY_IPHONE
-            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
-        #else
-            string adUnitId = "unexpected_platform";
-        #endif
+        Debug.Log("Creating banner view");
 
-        // Create a 320x50 banner at the top of the screen.
-        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        // If we already have a banner, destroy the old one.
+        if (_bannerView != null)
+        {
+            DestroyBannerView();
+        }
 
+        // Create a 320x50 banner at top of the screen
+        _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.Top);
         // Create an empty ad request.
-        AdRequest request = new AdRequest.Builder().Build();
-
+        AdRequest request = new AdRequest();
         // Load the banner with the request.
-        this.bannerView.LoadAd(request);
+        _bannerView.LoadAd(request);
     }
 
+    public void LoadAd()
+    {
+        // create an instance of a banner view first.
+        if(_bannerView == null)
+        {
+            CreateBannerView();
+        }
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+    }
+
+    public void DestroyBannerView()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("Destroying banner view.");
+            _bannerView.Destroy();
+            _bannerView = null;
+        }
+    }
+
+    private void ListenToAdEvents()
+    {
+        // Raised when an ad is loaded into the banner view.
+        _bannerView.OnBannerAdLoaded += () =>
+        {
+            Debug.Log("Banner view loaded an ad with response : "
+                + _bannerView.GetResponseInfo());
+        };
+        // Raised when an ad fails to load into the banner view.
+        _bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
+        {
+            Debug.LogError("Banner view failed to load an ad with error : "
+                + error);
+        };
+        // Raised when the ad is estimated to have earned money.
+        _bannerView.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(String.Format("Banner view paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        _bannerView.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Banner view recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        _bannerView.OnAdClicked += () =>
+        {
+            Debug.Log("Banner view was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        _bannerView.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Banner view full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        _bannerView.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Banner view full screen content closed.");
+        };
+    }
 }
